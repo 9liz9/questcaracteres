f44fa9292513dd15f0d4626d0503d2aad571112b
Liz Manresa <manresa.liz@gmail.com>
2023-06-02 11:39:54 +0200

    quest caracteres done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace S02_caracteres;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         for (int i = 97; i <= 122; i++)
+        {
+            char lettre = (char)i;
+            Console.WriteLine($"{lettre} = '{lettre}'");
+        }
+    }
+}
